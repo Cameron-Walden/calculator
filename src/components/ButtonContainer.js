6ed8ae45b1678ba6111a5121c802e8aedb800fb8
Cameron <cameronkwalden@gmail.com>
2022-11-11 14:19:11 -0800

    renders clicked number

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -1,12 +1,17 @@
-import { buttonValues } from "../buttonValues"
-import Button from "./Button"
+import { buttonValues } from "../buttonValues";
+import Button from "./Button";
 
-export default function ButtonContainer(){
-    return(
-        <div>
-            {buttonValues.flat().map((button, idx) => (
-                <Button button={button} idx={idx}/>
-            ))}
-        </div>
-    )
+export default function ButtonContainer({ calculator, setCalculator }) {
+  return (
+    <div>
+      {buttonValues.flat().map((button, idx) => (
+        <Button
+          button={button}
+          key={idx}
+          calculator={calculator}
+          setCalculator={setCalculator}
+        />
+      ))}
+    </div>
+  );
 }
