c3571414ea9ce7378d3cf0e630e974bff2173e23
Cameron <cameronkwalden@gmail.com>
2022-11-13 14:55:56 -0800

    can successfully subtract

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,7 +13,7 @@ export default function Button({ button, calculator, setCalculator }) {
 
   const resetCalculator = () => {
     setCalculator({
-      sign: "",
+      sign: "C",
       number: 0,
       result: 0,
     });
@@ -30,28 +30,41 @@ export default function Button({ button, calculator, setCalculator }) {
     });
   };
 
+  const selectSubtraction = () => {
+    setCalculator({
+      sign: "-",
+      number: 0,
+      result:
+        !calculator.result && calculator.number
+          ? calculator.number
+          : calculator.result,
+    });
+  };
+
   const selectEquals = () => {
-      const getTotal = (a, b, sign) => {
-        const total = {
-          "+": (a, b) => a + b,
-        };
-        console.log(total, "total");
-        console.log(total[sign], "total sign");
-        console.log(total[sign](a, b), "total sign ab");
-        return total[sign](a, b);
+    const getTotal = (a, b, sign) => {
+      const total = {
+        "+": (a, b) => a + b,
+        '-': (a,b) => a - b,
       };
-      console.log(calculator.result, "calc res");
-      setCalculator({
-        sign: "",
-        number: 0,
-        result: getTotal(calculator.result, calculator.number, calculator.sign),
-      });
+      console.log(total, "total");
+      console.log(total[sign], "total sign");
+      console.log(total[sign](a, b), "total sign ab");
+      return total[sign](a, b);
+    };
+    console.log(calculator.result, "calc res");
+    setCalculator({
+      sign: "",
+      number: 0,
+      result: getTotal(calculator.result, calculator.number, calculator.sign),
+    });
   };
 
   const handleAllButtons = () => {
     const buttonValues = {
       C: resetCalculator,
-      '+': selectAddition,
+      "+": selectAddition,
+      "-": selectSubtraction,
       "=": selectEquals,
     };
     if (buttonValues[button]) {
